60201ff34c668f374a589e5c0d438584f418aab3
Claudio-Pri <priv.claudio@gmail.com>
2024-07-22 16:34:22 +0200

    creati elementi in js con ciclo for

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,15 @@
 console.log('js collegato');
-
 //===== Appunti ======
 // ` backtick
 
 //===== Code ======
-
-
+//prendo il mio container dall'html
+const gridContainer = document.getElementById('grid-container');
+// console.log(gridContainer, typeof gridContainer);
+// genero l'elemento
+for (let i = 0; i < 100; i++) {
+    let newCell = document.createElement('div');
+    newCell.innerHTML = (i + 1);
+    gridContainer.append(newCell);
+}
 //===== Functions =====
